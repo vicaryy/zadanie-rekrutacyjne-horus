043079d5a213a3ba11a2b0dee0fdd5600601e348
vicary <vicarycholewa@gmail.com>
2024-08-08 13:08:04 +0200

    Add findBlocksByMaterial and count methods and tests for them

--- src/main/java/org/vicary/Wall.java
+++ src/main/java/org/vicary/Wall.java
@@ -1,6 +1,7 @@
 package org.vicary;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,14 +14,11 @@ public class Wall implements Structure {
 
     @Override
     public Optional<Block> findBlockByColor(String color) {
-        if (color == null)
-            return Optional.empty();
-
         return findBlockByColor(color, this.blocks);
     }
 
     private Optional<Block> findBlockByColor(String color, List<Block> blocks) {
-        if (blocks == null)
+        if (blocks == null || color == null)
             return Optional.empty();
 
         for (Block block : blocks) {
@@ -41,6 +39,9 @@ public class Wall implements Structure {
     }
 
     private List<Block> findBlocksByMaterial(String material, List<Block> foundBlocks, List<Block> blocks) {
+        if (blocks == null || material == null)
+            return Collections.emptyList();
+
         for (Block block : blocks) {
             if (block.material() != null && block.material().equals(material))
                 foundBlocks.add(block);
@@ -52,27 +53,19 @@ public class Wall implements Structure {
 
     @Override
     public int count() {
-        return count(this.blocks, 0);
+        return count(this.blocks);
     }
 
-    private int count(List<Block> blocks, int count) {
-        
-        return count;
+    private int count(List<Block> blocks) {
+        int amount = 0;
+        if (blocks == null)
+            return amount;
+
+        for (Block block : blocks) {
+            amount++;
+            if (block instanceof CompositeBlock compositeBlock)
+                amount += count(compositeBlock.blocks());
+        }
+        return amount;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
